abae629a0956a6da031e858877891aed76ad9a63
kyle-neal <nealkyle5@gmail.com>
2016-08-07 19:16:47 -0400

    Initial Commit
    Current functionality:
    - Add ssh logins to text box
    - Data transformed and displayed in the data grid view

---
+++ script_runner/MainWindow.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace script_runner
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.dataEntry = new System.Windows.Forms.RichTextBox();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.runButton.Location = new System.Drawing.Point(206, 419);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataEntry
+            // 
+            this.dataEntry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataEntry.Location = new System.Drawing.Point(12, 12);
+            this.dataEntry.Name = "dataEntry";
+            this.dataEntry.Size = new System.Drawing.Size(472, 180);
+            this.dataEntry.TabIndex = 1;
+            this.dataEntry.Text = "";
+            this.dataEntry.TextChanged += new System.EventHandler(this.dataEntry_TextChanged);
+            // 
+            // dataView
+            // 
+            this.dataView.AllowUserToAddRows = false;
+            this.dataView.AllowUserToDeleteRows = false;
+            this.dataView.AllowUserToOrderColumns = true;
+            this.dataView.AllowUserToResizeColumns = false;
+            this.dataView.AllowUserToResizeRows = false;
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataView.Location = new System.Drawing.Point(12, 198);
+            this.dataView.MultiSelect = false;
+            this.dataView.Name = "dataView";
+            this.dataView.ReadOnly = true;
+            this.dataView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataView.Size = new System.Drawing.Size(472, 202);
+            this.dataView.TabIndex = 2;
+            this.dataView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 464);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.dataEntry);
+            this.Controls.Add(this.runButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainWindow";
+            this.Text = "Bash Script Runner";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.RichTextBox dataEntry;
+        private System.Windows.Forms.DataGridView dataView;
+    }
+}
+
---
+++ script_runner/MainWindow.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace script_runner
+{
+    public partial class MainWindow : Form
+    {
+        // Variables
+        private String dataEntryHelp = "Copy/Paste SSH info here seperated by a delimeter....\nIE)\nssh - p < PORT > < USER >@< IP >,< SSH_PASSWORD >\n\nNOTE: only accepts 2 columns.";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataEntry.Text = dataEntryHelp;
+            dataEntry.ForeColor = Color.Gray;
+            dataEntry.GotFocus += new EventHandler(dataEntry_GotFocus);
+            dataEntry.LostFocus += new EventHandler(dataEntry_LostFocus);
+            dataView.ColumnCount = 2;
+            dataView.Columns[0].Name = "SSH Login";
+            dataView.Columns[1].Name = "SSH Password";
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Console.Beep();
+        }
+
+        private void dataEntry_TextChanged(object sender, EventArgs e)
+        {
+            
+            dataView.Rows.Clear();
+            dataView.Refresh();
+            String[] lines = this.dataEntry.Text.Split(new String[] { "\r\n", "\n" }, StringSplitOptions.None);
+            String delimeter = getDelimeter(lines);
+            for (int x = 0; x < lines.Length; x++)
+            {
+                String[] line = lines[x].Split(new String[] { delimeter }, StringSplitOptions.None);
+                System.Console.WriteLine("Length: " + line.Length);
+                this.dataView.Rows.Add(lines[x].Split(new String[] { delimeter }, StringSplitOptions.None));
+            }
+        }
+
+        void dataEntry_LostFocus(object sender, EventArgs e)
+        {
+            if (dataEntry.Text.Equals(string.Empty))
+            {
+                dataEntry.Text = dataEntryHelp;
+                dataEntry.ForeColor = Color.Gray;
+            }
+        }
+
+        void dataEntry_GotFocus(object sender, EventArgs e)
+        {
+            if (dataEntry.Text.Equals(dataEntryHelp))
+            {
+                dataEntry.Text = string.Empty;
+                dataEntry.ForeColor = Color.Black;
+            }
+        }
+
+        /// <summary>
+        /// This function takes in a csv in a string[] format, and determines the delimeter
+        /// </summary>
+        /// <param name="csvRow"></param>
+        /// <returns></returns>
+        public String getDelimeter(string[] csvRows)
+        {
+            // get counts of different possible seperators and compare.
+            int num_tabs = 0;
+            int num_commas = 0;
+            int num_pipes = 0;
+            int num_semicolons = 0;
+
+            for (int i = 0; i < 1000 && i < csvRows.Length; i++)
+            {
+                num_commas += csvRows[i].Count(f => f == ',');
+                num_tabs += csvRows[i].Count(f => f == '\t');
+                num_pipes += csvRows[i].Count(f => f == '|');
+                num_semicolons += csvRows[i].Count(f => f == ';');
+            }
+
+            if (num_commas > num_tabs && num_commas > num_pipes && num_commas > num_semicolons)
+            {
+                return ",";
+            }
+            else if (num_tabs > num_commas && num_tabs > num_pipes && num_tabs > num_semicolons)
+            {
+                return "\t";
+            }
+            else if (num_pipes > num_commas && num_pipes > num_tabs && num_pipes > num_semicolons)
+            {
+                return "|";
+            }
+            else
+            {
+                return ";";
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
